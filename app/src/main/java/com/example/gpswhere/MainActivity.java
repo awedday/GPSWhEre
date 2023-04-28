42287b80e61e3b477d3b1263cae1164119f8d033
Nastya Benzenko <isip_a.p.benzenko@mpt.ru>
2023-04-28 10:52:22 +0300

    Вх,рег,круг,обои,местополож

--- app/src/main/java/com/example/gpswhere/MainActivity.java
+++ app/src/main/java/com/example/gpswhere/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.gpswhere;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,31 +24,46 @@ public class MainActivity extends AppCompatActivity {
 
     final Random random = new Random();
     int Wallpaper;
+    Button signIn,signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Wallpaper = random.nextInt(5);
+        signUp = (Button)findViewById(R.id.signup);
+        signIn = (Button)findViewById(R.id.signin);
         RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.rootLayout);
+
+
 
 
 
         switch (Wallpaper){
             case 1:
-                relativeLayout.setBackgroundResource(R.drawable.japan);
+                relativeLayout.setBackgroundResource(R.drawable.wallspapermainactivityfive);
+                signIn.setBackgroundColor(getResources().getColor(R.color.buttonmainfive, null));
+                signUp.setBackgroundColor(getResources().getColor(R.color.buttonmainfive, null));
                 break;
             case 2:
-                relativeLayout.setBackgroundResource(R.drawable.usa);
+                relativeLayout.setBackgroundResource(R.drawable.wallspapermainactivitytwo);
+                signIn.setBackgroundColor(getResources().getColor(R.color.buttonmaintwo, null));
+                signUp.setBackgroundColor(getResources().getColor(R.color.buttonmaintwo, null));
                 break;
             case 3:
-                relativeLayout.setBackgroundResource(R.drawable.russia);
+                relativeLayout.setBackgroundResource(R.drawable.wallspapermainactivitythree);
+                signIn.setBackgroundColor(getResources().getColor(R.color.buttonmainthree, null));
+                signUp.setBackgroundColor(getResources().getColor(R.color.buttonmainthree, null));
                 break;
             case 4:
-                relativeLayout.setBackgroundResource(R.drawable.paris);
+                relativeLayout.setBackgroundResource(R.drawable.wallspapermainactivityfour);
+                signIn.setBackgroundColor(getResources().getColor(R.color.buttonmainfour, null));
+                signUp.setBackgroundColor(getResources().getColor(R.color.buttonmainfour, null));
                 break;
+
         }
 
+        System.out.print(Wallpaper);
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
         if (user == null){
